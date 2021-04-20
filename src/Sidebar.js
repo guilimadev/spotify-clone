51753ee6756e6f402718c9c25c22a667acee8cfa
Guilherme <guilimadev@gmail.com>
2021-04-20 11:20:34 -0300

    Ready to deploy

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,7 +12,7 @@ const [{playlists}, dispatch] = useDataLayerValue();
 
     return (
         <div className="sidebar">
-            <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+            <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=" " />
             
             <SidebarOption Icon={HomeIcon} title="Home" />
             <SidebarOption Icon={SearchIcon}  title="Search" />
